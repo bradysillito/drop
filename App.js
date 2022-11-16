a5945c698b2a95d07a3e8857b61fae322a82cdfe
Brady Sillito <sillito121@gmail.com>
2022-11-15 19:49:45 -0700

    Tab Bar Working

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, Image, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+//Import Screens
+import ProfileScreen from "./screens/ProfileScreen";
+import CreateScreen from "./screens/CreateScreen";
+import ClosetScreen from "./screens/ClosetScreen";
+
+//Import Materials for the Bottom Tab
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
+import { NavigationContainer} from '@react-navigation/native';
+
+//Import Icons for the NavBar
+import  MaterialCommunityIcons  from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App(){
+  return(
+      <NavigationContainer>
+        <Tab.Navigator 
+        labeled = {false}
+        barStyle ={{ backgroundColor: '#454545'}}
+        activeColor = "white"
+         >
+          <Tab.Screen 
+            name = "Closet"  
+            component={ClosetScreen}
+            options = {{tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name = "hanger" color={color} size = {35} style = {styles.icon}/>
+            ),}}/>
+         
+          
+          <Tab.Screen 
+            name = "Create"  
+            component={CreateScreen}
+            options = {{tabBarIcon: ({color,size}) => (
+              <MaterialCommunityIcons name = "bookmark-plus-outline" color={color} size = {35} style = {styles.icon}/>
+            ),}}/>
+         
+          <Tab.Screen
+            name = "Profile"  
+            component = {ProfileScreen}
+            options = {{tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name = "account-circle" color={color} size ={35} style = {styles.icon}/>
+            ),
+            }}/>
+
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    icon:{
+      height: 40,
+      width: 40,
+    },
 });
