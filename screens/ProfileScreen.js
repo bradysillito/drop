07cb14daf55ce16d3ffc70dd01cbead0cf10e18d
Brady Sillito <sillito121@gmail.com>
2022-11-22 19:38:14 -0700

    Finalized Profile Screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,51 @@
 import React from "react";
-import { Button, View, StyleSheet, Text } from 'react-native'
+import { FlatList, TouchableOpacity, View, StyleSheet, Text, Image, SafeAreaView } from 'react-native';
+import PlaceHolderImage from "../assets/data/PlaceHolderImage";
+import BlankImageGallery from "../components/BlankImageGallery";
+
+
+const ListImage = (props) => {
+    return(
+        <TouchableOpacity onPress={() => props.navigation.navigate("Closet")}>
+            <Image
+                source = {props.image.background}
+            />
+        </TouchableOpacity>
+       
+    );
+}
+
 
 const ProfileScreen = ({navigation}) => {
+    let images = PlaceHolderImage;
     return(
         <View style = {styles.container}>
-            <Text>
-                Profile Screen
-            </Text>
-            <Button title="Click Me" onPress={()=> alert("You Clicked the Button")}/>
+            <SafeAreaView>
+
+                {/* Top Navigation with Edit??*/}
+                
+                <View style = {styles.profileTitle}>
+                    <Image
+                        source={require('../assets/images/Profile.png')}
+                        style = {styles.ProfileImage}
+                    />
+                    <View style = {styles.nameWrapper}>
+                        <Text style = {styles.names}>Your</Text>
+                        <Text style = {styles.names}>Name</Text>
+                    </View>
+                </View>
+            </SafeAreaView>
+
+             <View style = {styles.galleryContainer}>
+                <FlatList 
+                    data={images}
+                    keyExtractor = {item => item.id}
+                    numColumns = {2}
+                    horizontal = {false}
+                    scrollEnabled = {true}
+                    renderItem = {({item}) => <ListImage image={item} navigation = {navigation} />}
+                />
+            </View>
         </View>
     );
 }
@@ -17,8 +55,33 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
+       backgroundColor: '#2C2C2C'
     },
+    galleryContainer:{
+        alignItems: 'center'
+    },
+    profileTitle:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginHorizontal: 25,
+        paddingBottom: 25,
+        borderBottomColor: '#6D6D6D',
+        borderBottomWidth: 2,
+    },
+    ProfileImage:{
+        height: 125,
+        width: 125,
+        marginRight: 15,
+        borderRadius: 130,
+        borderColor: '#C39581',
+        borderWidth: 6, 
+    },
+    names:{
+        color: 'white',
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+
 });
